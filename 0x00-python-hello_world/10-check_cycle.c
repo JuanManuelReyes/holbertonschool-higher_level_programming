de253d41e4327520857837308d74b939d829a593
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-25 10:12:20 -0700

    asd

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,5 +7,23 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *two = list;
+	listint_t *one = list;
 
+	if (list == NULL)
+		return (0);
+
+	while (1)
+	{
+		if (two->next != NULL && two->next->next != NULL)
+		{
+			two = two->next->next;
+			one = one->next;
+
+			if (two == one)
+				return (1);
+		}
+		else
+			return (0);
+	}
 }
