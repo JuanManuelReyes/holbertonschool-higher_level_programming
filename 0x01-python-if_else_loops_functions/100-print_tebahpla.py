a0d83b3a7527acbb50c2ea96303e504b9103f0a4
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-26 12:16:13 -0700

    asd

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 for i in range(0, 26):
+
     char = ord('z') - i
+
     if i % 2 == 1:
-        char = (chr(char - 97 + 65)
+        char = chr(char - ord('a') + ord('A'))
     else:
-    char = chr(char)
+        char = chr(char)
 
     print(f"{char}", end='')
