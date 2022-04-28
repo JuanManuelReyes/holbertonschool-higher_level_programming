d7ffa6a6b796875af41853cd3a8d56f046655793
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-28 06:21:27 -0700

    Update

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+        import hidden_4
+        dir = dir(hidden_4)
+        for row in dir:
+                if row[0] != '_':
+                        print(row)
