9886b9024891e9b08aef1ddff71506fdf4338111
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-28 08:19:44 -0700

    asd

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(sys.argv) <= 1:
-        print(0)
-    else:
-        aux = 0
-        res = 0
-        for i in range(1, len(sys.argv)):
-            aux = int(sys.argv[i])
-            res = res + aux
-            print(res)
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += (int(argv[i]))
+    print(sum)
