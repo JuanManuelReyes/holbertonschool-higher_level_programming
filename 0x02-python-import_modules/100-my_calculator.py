29b1d0fe2d9709038ea15403b4913d789805fd48
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-28 11:45:55 -0700

    asd

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        from calculator_1 import add, mul, sub, div
-        import sys
-        if len(sys.argv) != 4:
-                print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-                exit(1)
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        op = sys.argv[2]
-        if op == "+":
-                print(f"{a} {op} {b} = {add(a,b)}")
-        elif op == '*':
-                print(f"{a} {op} {b} = {mul(a, b)}")
-        elif op == "-":
-                print(f"{a} {op} {b} = {sub(a,b)}")
-        elif op == "/":
-                print(f"{a} {op} {b} = {div(a,b)}")
-        else:
-                print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+    from calculator_1 import add, mul, sub, div
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    if op == "+":
+        print(f"{a} {op} {b} = {add(a,b)}")
+    elif op == '*':
+        print(f"{a} {op} {b} = {mul(a, b)}")
+    elif op == "-":
+        print(f"{a} {op} {b} = {sub(a,b)}")
+    elif op == "/":
+        print(f"{a} {op} {b} = {div(a,b)}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
