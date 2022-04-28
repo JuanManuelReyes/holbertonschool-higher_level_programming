eaaabca39a55ce2fc0f7ae798f622bf6f3bd44ad
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-28 11:44:31 -0700

    asd

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
         from calculator_1 import add, mul, sub, div
         import sys
-        if (len(sys.argv) - 1 )!= 3:
+        if len(sys.argv) != 4:
                 print("Usage: ./100-my_calculator.py <a> <operator> <b>")
                 exit(1)
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        operand = sys.argv[2]
+        op = sys.argv[2]
         if op == "+":
                 print(f"{a} {op} {b} = {add(a,b)}")
         elif op == '*':
