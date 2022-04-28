32e8463eac961bcd50263973a49f72a92fe8dc84
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-04-28 08:05:32 -0700

    asd

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
         import sys
-
         if len(sys.argv) <= 1:
-            print(0);
+            print(0)
         else:
             aux = 0
             res = 0
