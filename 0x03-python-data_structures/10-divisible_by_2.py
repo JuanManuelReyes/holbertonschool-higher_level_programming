d44c874f3e73843b0271dd26c00ba38cf8aed4ce
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-02 10:22:20 -0700

    asd

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if my_list:
         divisible = my_list.copy()
         for i in range(len(my_list)):
-            if my_list[i] % 2 is 0:
+            if my_list[i] % 2 == 0:
                 divisible[i] = True
             else:
                 divisible[i] = False
