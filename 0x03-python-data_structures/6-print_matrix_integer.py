e2d4fed300b86b5bbd93587bd1fbb55086f38a67
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-02 08:23:49 -0700

    asd

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for x in i:
+            print(f"{x}", end="")
+            if x != i[-1]:
+                print(end=" ")
+        print("$")
