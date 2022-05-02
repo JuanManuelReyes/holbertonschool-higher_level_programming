5c41ada22c499810f462a7af221d0cfea9085327
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-02 09:35:38 -0700

    asd

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        maxint = my_list[0]
+        for i in my_list:
+            if i > maxint:
+                maxint = i
+        return maxint
+    else:
+        return None
