417ff1ce8c67234cfd5c5bd782689f9d2f0032ea
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-02 11:56:42 -0700

    asd

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,7 +11,7 @@ int is_palindrome(listint_t **head)
 {
         listint_t *aux = *head;
         unsigned int size = 0, i = 0;
-	int data[1024];
+	int data[10240];
 
         /*No existe LL*/
         if (head == NULL)
