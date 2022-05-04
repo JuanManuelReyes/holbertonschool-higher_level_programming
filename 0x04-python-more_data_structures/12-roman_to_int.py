75ca4d5effa3059c2f85139a1ec18a77365837f9
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-04 09:47:38 -0700

    asd

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,3 @@
-"""
 #!/usr/bin/python3
 
 def equivalents(r):
@@ -16,7 +15,7 @@ def equivalents(r):
         return 500
     if (r == 'M'):
         return 1000
-    return 0 
+    return 0
 
 
 def roman_to_int(roman_string):
@@ -43,22 +42,3 @@ def roman_to_int(roman_string):
             i = i + 1
 
     return res
-"""
-
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-    if roman_string == "":
-        return 0
-    num = 0
-    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    for i, j in zip(roman_string, roman_string[1:]):
-        if i not in dic.keys():
-            return 0
-        elif dic[i] >= dic[j]:
-            num += dic[i]
-        else:
-            num -= dic[i]
-    num += dic[roman_string[-1]]
-    return num
