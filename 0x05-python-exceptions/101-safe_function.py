a11f592f424965ce63f2f129688f5a99c517b1b5
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-16 08:44:54 -0700

    a

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         res = fct(*args)
         return res
-    except (Exception) as error:
-        sys.stderr.write("Exception {}\n".format(error))
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
         return None
