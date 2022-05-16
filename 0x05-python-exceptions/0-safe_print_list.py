4a5dface9a78b00ae9f42a24ef8c7fade3f00d5c
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-16 07:52:46 -0700

    All

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#/usr/bon/python3
+def safe_print_list(my_list=[], x=0):
+    element_count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            element_count += 1
+        except IndexError:
+            break
+    print()
+    return element_count
