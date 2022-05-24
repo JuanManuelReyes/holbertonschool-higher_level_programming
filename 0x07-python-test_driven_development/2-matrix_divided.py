5c46453a2cbdb0315f531713a7e94f48d2fbdc87
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-24 11:56:24 -0300

    New Update34.0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,19 +4,24 @@
 
 def matrix_divided(matrix, div):
     """asd asd asd """
+    
     errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
         raise TypeError(errorMessage)
+
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
     new_matrix = []
     for lists in matrix:
         if type(lists) is not list:
             raise TypeError(errorMessage)
         if len(lists) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
+
         new_list = []
         for position in lists:
             if type(position) is not int and type(position) is not float:
