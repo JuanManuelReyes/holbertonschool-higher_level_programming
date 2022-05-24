dd72f979e79ea13b1004f091de91284ca2e784e8
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-24 15:26:28 -0300

    New Update73.0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,15 +3,15 @@
 
 
 def print_square(size):
-        """asd asd asd"""
+    """asd asd asd"""
 
-        if type(size) is not int:
-                raise TypeError("size must be an integer")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
         
-        if size < 0:
-                raise ValueError("size must be >= 0")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
-        for x in range(size):
-                for y in range(size):
-                        print("#", end="")
-                print()
+    for x in range(size):
+        for y in range(size):
+            print("#", end="")
+        print()
