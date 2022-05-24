e7adff394d59fd86db083913412bc03160b40696
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-24 11:57:55 -0300

    New Update36.0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,10 +22,10 @@ def matrix_divided(matrix, div):
         if len(lists) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
 
-        list = []
+        new_list = []
         for position in lists:
             if type(position) is not int and type(position) is not float:
                 raise TypeError(error_msg)
-            list.append(round(position/div, 2))
-        mtrx.append(list)
+            new_list.append(round(position/div, 2))
+        mtrx.append(new_list)
     return mtrx
