17b1375643514d60a9961aca50117170dc4d5b3a
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-23 21:38:58 -0300

    New Update19.0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,29 +5,24 @@
 def matrix_divided(matrix, div):
         """asd asd asd"""
 
+        errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
+        if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
+                raise TypeError(errorMessage)
         if type(div) is not int and type(div) is not float:
                 raise TypeError("div must be a number")
-
-        error_msg = "matrix must be a matrix (list of lists) of integers/floats"
-        if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
-                raise TypeError(error_msg)
-        
-        if div == 0:
+         if div == 0:
                 raise ZeroDivisionError("division by zero")
-
-        new_mtrx = []
+        new_matrix = []
         for lists in matrix:
                 if type(lists) is not list:
-                        raise TypeError(error_msg)
+                        raise TypeError(errorMessage)
                 if len(lists) != len(matrix[0]):
                         raise TypeError("Each row of the matrix must have the same size")
 
         new_list = []
         for position in lists:
             if type(position) is not int and type(position) is not float:
-                raise TypeError(error_msg)
+                raise TypeError(errorMessage)
             new_list.append(round(position/div, 2))
-        
-        new_mtrx.append(new_list)
-
-        return new_mtrx
+        new_matrix.append(new_list)
+        return new_matrix
