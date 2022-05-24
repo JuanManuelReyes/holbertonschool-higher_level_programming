b1690f545f7692fe8bb6fc03ab72553ad07516c1
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-23 21:44:25 -0300

    New Update22.0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,26 +3,25 @@
 
 
 def matrix_divided(matrix, div):
-        """asd asd asd"""
+    """function that divide all the values of the matrix"""
+    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if matrix is None or len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError(err_msg)
+    if len(matrix[0]) != len(matrix[1]):
+        raise TypeError("Each row of the matrix must have the same size")
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-        errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
-        if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
-                raise TypeError(errorMessage)
-        if type(div) is not int and type(div) is not float:
-                raise TypeError("div must be a number")
-        if div == 0:
-                raise ZeroDivisionError("division by zero")
-        new_matrix = []
-        for lists in matrix:
-                if type(lists) is not list:
-                        raise TypeError(errorMessage)
-                if len(lists) != len(matrix[0]):
-                        raise TypeError("Each row of the matrix must have the same size")
-
-        new_list = []
-        for position in lists:
-            if type(position) is not int and type(position) is not float:
-                raise TypeError(errorMessage)
-            new_list.append(round(position/div, 2))
-        new_matrix.append(new_list)
-        return new_matrix
+    results_matrix = []
+    for listas in matrix:
+        if type(listas) != list:
+            raise TypeError(err_msg)
+        c_resList = []
+        for valor in listas:
+            if type(valor) != int and type(valor) != float:
+                raise TypeError(err_msg)
+            c_resList.append(round(valor/div, 2))
+        results_matrix.append(c_resList)
+    return results_matrix
