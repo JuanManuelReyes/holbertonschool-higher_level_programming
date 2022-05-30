97146e67258d630b8c6711314b11a20add90261a
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-30 15:50:02 -0700

    AllTested

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,8 @@
 
 def inherits_from(obj, a_class):
     """asd asd asd"""
-    if issubclass(obj, a_class) and type(obj) != a_class:
+
+    if (type(obj) is not a_class and issubclass(type(obj), a_class)):
         return True
     else:
         return False
