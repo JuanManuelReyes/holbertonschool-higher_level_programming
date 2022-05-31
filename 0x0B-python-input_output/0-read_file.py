879d58061f6aa784660272fe19e0b65778c8c361
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-31 13:34:17 -0700

    now

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename="", mode="r", encoding="utf-8"):
     """asd asd asd"""
     with open(filename) as _file:
         lines = _file.read()
-        print(lines)
+        print(lines, end="")
