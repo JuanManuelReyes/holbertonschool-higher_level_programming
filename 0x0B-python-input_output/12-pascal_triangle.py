358b8121891837b90d708a3ee013874fd336b34e
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-31 18:42:53 -0300

    asd

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,11 +9,11 @@ def pascal_triangle(n):
         return [[1]]
 
     pt=[1] 
-    for i in range(n): 
-        print(pt) 
-        newlist=[] 
-        newlist.append(pt[0]) 
-        for i in range(len(pt)-1): 
-            newlist.append(pt[i]+pt[i+1]) 
-        newlist.append(pt[-1]) 
-        pt=newlist 
+    for i in range(n):
+        print(pt)
+        newlist=[]
+        newlist.append(pt[0])
+        for i in range(len(pt)-1):
+            newlist.append(pt[i]+pt[i+1])
+        newlist.append(pt[-1])
+        pt=newlist
