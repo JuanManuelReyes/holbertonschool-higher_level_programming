99df9f5b59b533eab90a1bf56a0856470a75618d
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-31 13:45:34 -0700

    f

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import json
 """asd asd asd"""
 
 
 def save_to_json_file(my_obj, filename):
     """asd asd asd"""
-        
+
+    import json
+
     with open(filename, "w") as _file:
         return (_file.write(json.dumps(my_obj)))
