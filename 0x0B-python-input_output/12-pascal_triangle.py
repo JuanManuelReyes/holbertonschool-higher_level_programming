538d2b509bff0244833cfbdb930b425969f0dd3f
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-31 18:57:31 -0300

    asd

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """asd asd asd"""
 
+def pascal_triangle(n):
+    pt = []
 
+    for i in range(n):
+        size = []
+        if i is 0:
+            size.append(1)
+            pt = pt + size
+        else:
+            for a in range(i):
+                size
+
+"""
 def pascal_triangle(n):
 
     if n <= 0:
@@ -18,3 +30,4 @@ def pascal_triangle(n):
             newlist.append(pt[i] + pt[i + 1])
         newlist.append(pt[-1])
         pt = newlist
+"""
