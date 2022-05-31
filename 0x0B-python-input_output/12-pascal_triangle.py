23786ebb5abc36f091263d556d5f6d0fd26b9eb3
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-05-31 18:30:42 -0300

    asd

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,6 +3,34 @@
 
 """
 def pascal_triangle(n):
+    for i in range(1, n+1):
+        for j in range(0, n-i+1):
+            print(' ', end='')
+
+        C = 1
+        for j in range(1, i+1):
+            print(' ', C, sep='', end='')
+            C = C * (i - j) // j
+
+        print()
+"""
+
+"""
+from math import factorial
+ 
+def pascal_triangle(n):
+    for i in range(n):
+        for j in range(n-i+1):
+            print(end=" ")
+ 
+        for j in range(i+1):
+            print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+ 
+        print()
+"""
+
+
+def pascal_triangle(n):
     if n <= 0:
         return []
     elif n == 1:
@@ -10,20 +38,13 @@ def pascal_triangle(n):
 
     pt = [[1]]
 
-    for i in range(n - 1):
-        pt.append([a + b for a, b in zip([0] + pt[-1], pt[-1] + [0])])
+    for i in range(n - 1): 
+        print(pt) 
+        newlist=[] 
+        newlist.append(pt[0]) 
+        for i in range(len(pt)-1): 
+            newlist.append(pt[i]+pt[i+1]) 
+        newlist.append(pt[-1]) 
+        pt=newlist 
 
     return pt
-"""
-def pascal_triangle(n):
-    for i in range(1, n+1):
-        for j in range(0, n-i+1):
-            print(' ', end=''
-
-        C = 1
-        for j in range(1, i+1):
-
-            print(' ', C, sep='', end='')
-
-            C = C * (i - j) // j
-        print()
