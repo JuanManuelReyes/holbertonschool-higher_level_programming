bb4603eb141b56cd05436455b625182ca6fd6f53
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 11:29:48 -0300

    Task 9

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const arg = parseInt(process.argv[2]);
+const char = 'X';
+
+if (isNaN(arg) === false && typeof arg === 'number') {
+        for (let i = 0; i < arg; i++) {
+                console.log(char.repeat(arg));   
+        }
+} else {
+        console.log('Missing size');
+}
