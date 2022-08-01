08375f5c29ec36ed278a5db0d2de469d5477f65e
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 10:08:45 -0300

    Task 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+if (proccess.argv.length <= 2) {
+        console.log('No argument');
+} else if (proccess.argv.length === 3) {
+        console.log('Argument found');
+} else {
+        console.log('Argument found');
+}
