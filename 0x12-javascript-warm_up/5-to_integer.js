cc0ce3189d5a2ef5ec45f9de489e6c18f10e6fc2
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 11:00:35 -0300

    Task 6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const arg = parseInt(process.argv[2]);
+
+if (isNaN(arg) === false && typeof arg === 'number') {
+        console.log('My number: ' + arg);
+} else {
+        console.log('Not a number');
+}
