f84b72404a2d05d903d19c8fd1650f8ff423354e
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 11:36:24 -0300

    Task 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+function factorial (num) {
+        if (num < 2) {
+                return (1);
+        } else {
+                return (num * factorial(num - 1));
+        }
+}
+
+const arg = parseInt(process.argv[2]);
+
+if (isNaN(arg) === false && typeof arg === 'number') {
+        const res = factorial(arg);
+        console.log(res);
+} else {
+        console.log(1);
+}
