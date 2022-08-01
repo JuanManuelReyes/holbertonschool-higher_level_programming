f4db4e1dcccc96e2b0425bb15688adbc7c8571ae
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 20:44:20 -0300

    Fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-console.log(process.argv[2] + ' is ' + process.argv[2]);
+const process = require('process');
+const argv = process.argv;
+
+console.log(argv[2] + ' is ' + argv[2]);
