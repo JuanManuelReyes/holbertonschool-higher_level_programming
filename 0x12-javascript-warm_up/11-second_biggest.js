a5d9009b57a1858c75161f8dec3be95b611874d3
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 21:02:15 -0300

    Task 12

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const process = require('process');
+const argv = process.argv;
+
+if (argv.length <= 3) {
+        console.log(0);
+} else {
+        let counter = 2;
+        let max = 0, second_max = 0, aux = 0;
+        while (counter <= argv.length) {
+                if (parseInt(argv[counter]) > max) {
+                        aux = max;
+                        max = parseInt(argv[counter]);
+                } else if (aux > second_max) {
+                        second_max = aux;
+                } else if (parseInt(argv[counter]) > second_max) {
+                        second_max = parseInt(argv[counter]);
+                }
+                counter++;
+        }
+        console.log(second_max);
+
+
+}
