a136c272ea3b00319d0c4b69afe5f03c2e323d26
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 21:03:40 -0300

    Fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,20 +3,20 @@ const process = require('process');
 const argv = process.argv;
 
 if (argv.length <= 3) {
-        console.log(0);
+  console.log(0);
 } else {
-        let counter = 2;
-        let max = 0, second_max = 0, aux = 0;
-        while (counter <= argv.length) {
-                if (parseInt(argv[counter]) > max) {
-                        aux = max;
-                        max = parseInt(argv[counter]);
-                } else if (aux > second_max) {
-                        second_max = aux;
-                } else if (parseInt(argv[counter]) > second_max) {
-                        second_max = parseInt(argv[counter]);
-                }
-                counter++;
-        }
-        console.log(second_max);
+  let counter = 2;
+  let max = 0; const secondMax = 0; let aux = 0;
+  while (counter <= argv.length) {
+    if (parseInt(argv[counter]) > max) {
+      aux = max;
+      max = parseInt(argv[counter]);
+    } else if (aux > secondMax) {
+      secondMax = aux;
+    } else if (parseInt(argv[counter]) > secondMax) {
+      secondMax = parseInt(argv[counter]);
+    }
+    counter++;
+  }
+  console.log(secondMax);
 }
