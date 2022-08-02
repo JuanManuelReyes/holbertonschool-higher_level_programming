8f0cbaefe1c0ecd62aceff819103037e6cc7232f
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-02 12:29:22 -0300

    Not Working

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const array = require('./mains/100-data').list;
 
-const newList = list.map((x, index) => x * index);
-
-console.log(list);
-console.log(newList);
+const newArray = array.map((x, index) => x * index);
+console.log(array);
+console.log(newArray);
