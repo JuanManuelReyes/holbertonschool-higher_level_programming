b3515fdefe8237a810ce277e5aa196bd7c5f97b0
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-02 08:45:30 -0300

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const char = "X";
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (
+      w <= 0 ||
+      h <= 0 ||
+      typeof w === "undefined" ||
+      typeof h === "undefined"
+    ) {
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+};
