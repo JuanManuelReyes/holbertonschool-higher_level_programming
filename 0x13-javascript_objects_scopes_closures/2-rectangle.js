23b83f4f481d5dbbd01316990cb98868ef3a7e00
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-02 12:06:29 -0300

    Semistandar

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w === 'undefined' || typeof h === 'undefined') {
-
+    if (
+      w <= 0 ||
+      h <= 0 ||
+      typeof w === 'undefined' ||
+      typeof h === 'undefined'
+    ) {
     } else {
       this.width = w;
       this.height = h;
