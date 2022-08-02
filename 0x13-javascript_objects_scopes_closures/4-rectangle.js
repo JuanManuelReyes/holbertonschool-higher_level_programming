b6f6c3379516457e7a71a91b5b0c188da9c461f6
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-02 12:52:36 -0300

    fixed

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,16 +2,11 @@
 const char = 'X';
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (
-      w <= 0 ||
-      h <= 0 ||
-      typeof w === 'undefined' ||
-      typeof h === 'undefined'
-    ) {
-    } else {
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || h <= 0 || typeof w === 'undefined' || typeof h === 'undefined') {
+      return;
     }
+    this.width = w;
+    this.height = h;
   }
 
   print () {
