852ac058ae5a27762a4454e35761a5a2d9ae73a1
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-02 09:40:30 -0300

    Task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let quantity = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      quantity++;
+    }
+  }
+  
+  return quantity;
+};
