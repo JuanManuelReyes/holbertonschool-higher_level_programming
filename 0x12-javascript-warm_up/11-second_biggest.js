8d86f2241adf335da3b80655a3fb4333097a1764
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-01 21:05:01 -0300

    Fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,17 +6,17 @@ if (argv.length <= 3) {
   console.log(0);
 } else {
   let counter = 2;
-  let max = 0; let second_max = 0; let aux = 0;
+  let max = 0; let secondMax = 0; let aux = 0;
   while (counter <= argv.length) {
     if (parseInt(argv[counter]) > max) {
       aux = max;
       max = parseInt(argv[counter]);
-    } else if (aux > second_max) {
-      second_max = aux;
-    } else if (parseInt(argv[counter]) > second_max) {
-      second_max = parseInt(argv[counter]);
+    } else if (aux > secondMax) {
+      secondMax = aux;
+    } else if (parseInt(argv[counter]) > secondMax) {
+      secondMax = parseInt(argv[counter]);
     }
     counter++;
   }
-  console.log(second_max);
+  console.log(secondMax);
 }
