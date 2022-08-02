d10aefff12c943e78cd9fd0331cd41edcad4d2f5
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-02 08:51:33 -0300

    Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+const char = "X";
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (
+      w <= 0 ||
+      h <= 0 ||
+      typeof w === "undefined" ||
+      typeof h === "undefined"
+    ) {
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    let aux = this.width;
+
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
