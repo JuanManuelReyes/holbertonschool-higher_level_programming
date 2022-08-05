f9777c9cccd64605b296cdfcc47fa822a8b27798
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-04 23:36:47 -0300

    Task4

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+        """Conection to the DataBase"""
+        db = MySQLdb.connect(host="localhost", port=3306, 
+                             user=argv[1], 
+                             password=argv[2], 
+                             db=argv[3])
+        
+        """Cursor excecutes the SQL queries"""
+        cursor = db.cursor()
+        command = "SELECT * FROM states WHERE name=%s ORDER BY id ASC;"
+        cursor.execute(command, (argv[4],))
+        rows = cursor.fetchall()
+        
+        for x in rows:
+                print(x)
+        
+        cursor.close()
+        db.close()
