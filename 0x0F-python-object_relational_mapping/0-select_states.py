ddaf33dffba1dce5815129c8cd252a2fa0b9422d
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-04 23:22:14 -0300

    Task1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+        """Conection to the DataBase"""
+        db = MySQLdb.connect(host="localhost", port=3306, 
+                             user=argv[1], 
+                             password=argv[2], 
+                             db=argv[3])
+        
+        """Cursor excecutes the SQL queries"""
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+        rows = cursor.fetchall()
+        
+        for x in rows:
+                print(x)
+        
+        cursor.close()
+        db.close()
