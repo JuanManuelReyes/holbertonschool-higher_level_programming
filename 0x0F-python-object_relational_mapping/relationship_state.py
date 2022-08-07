bee3a47130ff27110eb6ae24effb6c566e2554f2
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-07 00:04:14 -0300

    Fix

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -8,16 +8,36 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+
 Base = declarative_base()
 
 
 class State(Base):
-    """Class state"""
+    """
+    asd asd asd
+    """
+    __tablename__ = 'states'
 
-    __tablename__ = "states"
-
-    id = Column(Integer, nullable=False, primary_key=True)
-
+    id = Column(Integer, primary_key=True, nullable=False)
+    
     name = Column(String(128), nullable=False)
+    
+    cities = relationship('City', backref="state")
 
-    cities = relationship("City", backref="state")
+# from sqlalchemy import Column, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship
+
+# Base = declarative_base()
+
+
+# class State(Base):
+#     """Class state"""
+
+#     __tablename__ = "states"
+
+#     id = Column(Integer, nullable=False, primary_key=True)
+
+#     name = Column(String(128), nullable=False)
+
+#     cities = relationship("City", backref="state")
