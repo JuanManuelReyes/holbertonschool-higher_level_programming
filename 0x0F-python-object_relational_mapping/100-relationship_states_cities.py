a613b9ba554ccf030976dcf11408a7cbee516a65
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-07 00:05:57 -0300

    Fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,30 +3,57 @@
 write a script 14-model_city_fetch_by_state.py that prints
 all City objects from the database hbtn_0e_14_usa
 """
-
-from sys import argv
+import sys
 from relationship_state import Base, State
-from relationship_city import City
+from relationship_city import City  
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="California")
-    new_city = City(name="San Francisco")
+    California = State(name="California")
+    SanFrancisco = City(name="San Francisco")
 
-    new_state.cities.append(new_city)
+    California.cities.append(SanFrancisco)
 
-    session.add(new_state)
-    session.add(new_city)
+    session.add(California)
+    session.add(SanFrancisco)
 
     session.commit()
     session.close()
+
+# from sys import argv
+# from relationship_state import Base, State
+# from relationship_city import City
+# from sqlalchemy import (create_engine)
+# from sqlalchemy.orm import sessionmaker
+
+# if __name__ == "__main__":
+#     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+#                            .format(argv[1], argv[2], argv[3]),
+#                            pool_pre_ping=True)
+
+#     Base.metadata.create_all(engine)
+    
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+
+#     new_state = State(name="California")
+#     new_city = City(name="San Francisco")
+
+#     new_state.cities.append(new_city)
+
+#     session.add(new_state)
+#     session.add(new_city)
+
+#     session.commit()
+#     session.close()
