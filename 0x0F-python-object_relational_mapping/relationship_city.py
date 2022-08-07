78f51f2a48b6cf7d13ee05c3cf355c15f295e25f
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-07 00:10:44 -0300

    Fix

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,33 +11,12 @@ from relationship_state import Base, State
 
 
 class City(Base):
-    """
-    asd asd asd
-    """
-    __tablename__ = 'cities'
+    """Class City"""
     
-    id = Column(Integer, primary_key=True, nullable=False)
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, primary_key=True)
     
     name = Column(String(128), nullable=False)
-    
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
-
-# from sqlalchemy import Column, Integer, String, ForeignKey
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import relationship
-# from relationship_state import Base, State
-
-# Base = declarative_base()
-
-
-# class City(Base):
-#     """Class City"""
-    
-#     __tablename__ = 'cities'
-    
-#     id = Column(Integer, nullable=False, primary_key=True)
-    
-#     name = Column(String(128), nullable=False)
-    
-#     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
