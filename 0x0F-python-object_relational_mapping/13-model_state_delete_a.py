9275e7b1e7db29204610ec424b01d0fcf275a9c3
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-06 22:36:03 -0300

    Fix

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     session = Session()
 
     states = session.query(State).\
-    filter(State.name.like('%a%')).all()
+        filter(State.name.like('%a%')).all()
 
     for state in states:
         session.delete(state)
