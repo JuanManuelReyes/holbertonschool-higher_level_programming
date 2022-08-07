24b1653106699424e91686ef60cfb6d552f47456
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-07 13:31:28 -0300

    Task 17 & 18

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+write a script 14-model_city_fetch_by_state.py that prints
+all City objects from the database hbtn_0e_14_usa
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.querry(State).orde_by(State.id).all()
+    
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
+
+    session.close()
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+write a script 14-model_city_fetch_by_state.py that prints
+all City objects from the database hbtn_0e_14_usa
+"""
+from ntpath import join
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(State.name, City.id, City.name).join(
+        City).order_by(City.id).all()
+
+    for city_state in cities:
+        print(f"{city_state[1]}: {city_state[2]} -> {city_state[0]}")
+
+    session.close()
