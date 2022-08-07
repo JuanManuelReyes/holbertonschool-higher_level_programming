20d380e8f5f06a3522fb331ba2c00e1f55f1d127
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-06 23:56:54 -0300

    Fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,8 +5,8 @@ all City objects from the database hbtn_0e_14_usa
 """
 
 from sys import argv
-from relationship_state import State
-from relationship_city import City, Base
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -15,6 +15,8 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
+    
     Session = sessionmaker(bind=engine)
     session = Session()
 
