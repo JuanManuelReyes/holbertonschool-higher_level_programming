d6edd0780a447119c174d510e91be4948d320594
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-07 17:39:03 -0300

    Fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,7 +3,7 @@
 write a script 14-model_city_fetch_by_state.py that prints
 all City objects from the database hbtn_0e_14_usa
 """
-import sys
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import (create_engine)
@@ -12,8 +12,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+                           .format(argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
