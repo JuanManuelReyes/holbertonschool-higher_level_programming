64497c1a1bd5d72114dd303d579657a3efde6d4c
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-07 17:45:39 -0300

    Fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]),
+                           .format(argv[1], argv[2],argv[3]), 
                            pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
@@ -20,11 +20,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    data = session.query(State).order_by(State.id).all()
 
-    for state in states:
-        print(f"{states.id}: {states.name}")
-        for cities in states.cities:
-            print(f"    {cities.id}: {cities.name}")
+    for state in data:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
 
     session.close()
