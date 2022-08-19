65ffdfe5a647cc9323dc42ed9452e299ed43a21c
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-19 16:30:59 -0300

    Task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes 2 arguments
+in order to solve this challenge.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    repo = argv[1]
+    user = argv[2]
+    
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, user)
+
+    res = requests.get(url)
+    json = res.json()
+
+    print(json)
