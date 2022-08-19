28c4c1a16a75d9ce9a9f4d459c07ca771f4b230f
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-19 16:52:33 -0300

    Task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     for idx, commit in enumerate(json):
         if idx < 10:
-            print("{}: {}".format(commit.get('sha'), commit.get('author')
-                                  .get('name')))
+            print("{}: {}".format(commit.get('sha'),commit.get('commit')
+                                  .get('author').get('name')))
         else:
             exit
