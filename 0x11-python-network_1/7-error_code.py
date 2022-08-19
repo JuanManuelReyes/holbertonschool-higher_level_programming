9dc6389686fdb5139541901e1d3a932b83ae1d50
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-19 15:30:08 -0300

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a
+URL, sends a request to the URL and displays
+the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    req = requests.get(url)
+
+    if req:
+        print(req.text)
+    else:
+        print("Error code:", req.status_code)
