c392c627d8e84be5d82f9b85052c56c8b5315d26
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-19 16:11:10 -0300

    Task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub credentials
+(username and password)
+and uses the GitHub API to display your id
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    
+    user = argv[1]
+    token = argv[2]
+    data = (user, token)
+
+    res = requests.get('https://api.github.com/user', data)
+    json = res.json()
+    print(json)
