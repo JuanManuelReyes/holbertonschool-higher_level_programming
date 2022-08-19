be1e03b41e3cda77951aa2b9366623665adbcb48
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-19 15:38:40 -0300

    Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) > 1:
+        q = str(argv[1])
+    else:
+        q = ""
+    
+    data = {'q': q}
+    res = requests.post("http://0.0.0.0:5000/search_user", data)
+
+    try:
+        json = res.json()
+        if(len(json)) == 0:
+            print("NOAHYJSON")
+        if(len(json) > 0):
+            print(json)
+    except ValueError as error:
+        print("Not a valid JSON")
