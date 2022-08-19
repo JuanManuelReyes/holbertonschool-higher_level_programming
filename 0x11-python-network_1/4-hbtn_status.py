824c9316df18815c6bd0ef48cfd37ee7340cb72e
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-19 15:02:17 -0300

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" 
+Write a Python script that fetches
+https://intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(req.text))
+    print("\t- content:", req.text)
