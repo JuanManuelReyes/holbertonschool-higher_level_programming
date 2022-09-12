146836ca42f95a688d6f41f4267d7e114be63cc3
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 10:13:20 -0300

    Task 2 Done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+let file = process.argv[2]
+let string = process.argv[3]
+const fs = require('fs');
+
+fs.writeFile(file, string, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
