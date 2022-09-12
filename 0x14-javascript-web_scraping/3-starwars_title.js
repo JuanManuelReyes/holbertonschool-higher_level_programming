7c1cd4c5fe4a724a14b2fbe16de0512254000a44
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 15:35:25 -0300

    Fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
-const req = require('request');
+const axios = require('axios');
 
-req.get(url, (err, res) => {
-  if (err) {
-    console.log('code: ' + err.res.status);
-  }
+axios.get(url).then(res => {
   console.log(res.data.title);
-});
+}).catch(err => {
+  console.log('code: ' + err.res.status);
+})
