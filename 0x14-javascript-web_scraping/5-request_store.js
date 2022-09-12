099b1902adb19c0d790b0678da7ab6d640d48642
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 16:36:16 -0300

    Fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
 const url = process.argv[2];
+const file = process.argv[3];
 const axios = require('axios').default;
+const fs = require('fs');
 
 axios.get(url).then(function (res) {
   const data = res.data;
-  const dict = {};
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].completed === true) {
-      if (dict[data[i].userId] !== undefined) {
-        dict[data[i].userId] = dict[data[i].userId] += 1;
-      } else {
-        dict[data[i].userId] = 1;
-      }
+
+  fs.writeFile(file, data, err => {
+    if (err) {
+      console.error(err);
     }
-  }
-  console.log(dict);
+  });
 }).catch(function (error) {
   console.log(`code: ${error.response.status}`);
 });
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const url = process.argv[2];
+const axios = require('axios').default;
+
+axios.get(url).then(function (res) {
+  const data = res.data;
+  const dict = {};
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].completed === true) {
+      if (dict[data[i].userId] !== undefined) {
+        dict[data[i].userId] = dict[data[i].userId] += 1;
+      } else {
+        dict[data[i].userId] = 1;
+      }
+    }
+  }
+  console.log(dict);
+}).catch(function (error) {
+  console.log(`code: ${error.response.status}`);
+});
