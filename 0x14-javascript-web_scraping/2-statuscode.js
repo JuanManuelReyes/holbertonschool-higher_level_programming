fcda15ff8262b4dcf5ec54f9f47d56612e35b213
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 10:26:15 -0300

    Task 2 Done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+let url = process.argv[2]
+const req = require('request');
+
+fs.get(url, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('code: ' + res.statusCode);
+});
