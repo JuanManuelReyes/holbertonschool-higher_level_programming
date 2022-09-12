37f2cbdf2a6b24406cff9207f1b142e512dd193d
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 10:07:07 -0300

    Task 0 Done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+/*
+https://www.geeksforgeeks.org/node-js-process-argv-property/#:~:text=argv%20property%20is%20an%20inbuilt,run%20in%20the%20command%20line.&text=Return%20Value%3A%20This%20property%20returns,it%20in%20the%20command%20line.
+https://nodejs.dev/en/learn/reading-files-with-nodejs/
+
+*/
+let file = process.argv[2]
+const fs = require('fs');
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
