7796e8fd8ff036e325dbf7d64ae24173bb9de45d
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 16:02:26 -0300

    Fixa

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,17 +3,17 @@ const url = process.argv[2];
 const axios = require('axios').default;
 
 axios.get(url).then(function (res) {
-    const movies = res.data.results;
-    let counter = 0;
-    for (let i = 0; i < movies.length; i++) {
-      for (let x = 0; x < movies[i].characters.length; x++) {
-        if (movies[i].characters[x].includes("18")) {
-          counter++; 
+  const movies = res.data.results;
+  let counter = 0;
+  for (let i = 0; i < movies.length; i++) {
+    for (let x = 0; x < movies[i].characters.length; x++) {
+      if (movies[i].characters[x].includes("18")) {
+        counter++; 
           break; 
         }
       }
     }
-    console.log(counter);
+  console.log(counter);
   }).catch(function (error) {
     console.log(`code: ${error.response.status}`);
   });
