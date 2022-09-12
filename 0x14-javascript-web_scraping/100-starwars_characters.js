b67b9a47e5b08650b1eae90cb398458965b111c9
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 19:32:14 -0300

    Advanced task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+const axios = require('axios').default;
+
+axios.get(url).then(res => {
+  for (const char in res.data.characters) {
+    axios.get(res.data.characters[char]).then(res => {
+              console.log(res.data.name);
+            });
+        }
+  }).catch(function (error) {
+    console.log(`code: ${error.res.status}`);
+  });
+  
