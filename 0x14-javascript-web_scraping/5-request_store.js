22704251b2c19b5351cc912a725b7d2704ab2110
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 16:23:51 -0300

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const url = process.argv[2];
+const file = process.argv[3];
+const axios = require('axios').default;
+const fs = require('fs');
+
+axios.get(url).then(function (res) {
+  const data = res.data;
+
+  fs.writeFile(file, data, err => {
+    if (err) {
+        console.error(err);
+    }
+  });
+}).catch(function (error) {
+  console.log(`code: ${error.response.status}`);
+});
