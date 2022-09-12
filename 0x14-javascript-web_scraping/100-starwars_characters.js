fcab4b6b35960a0ef4271fd2f57d337b02b61697
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-12 19:33:07 -0300

    Fix semistandar

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,10 +6,9 @@ const axios = require('axios').default;
 axios.get(url).then(res => {
   for (const char in res.data.characters) {
     axios.get(res.data.characters[char]).then(res => {
-              console.log(res.data.name);
-            });
-        }
-  }).catch(function (error) {
-    console.log(`code: ${error.res.status}`);
-  });
-  
+      console.log(res.data.name);
+    });
+  }
+}).catch(function (error) {
+  console.log(`code: ${error.res.status}`);
+});
