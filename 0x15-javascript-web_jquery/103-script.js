c3e29cc2ed1a9dbf125de7227b85a4e6f0d7f74b
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-19 18:53:39 -0300

    Task 103

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,14 @@
-100-script.js
+document.addEventListener("DOMContentLoaded", function() {
+    $("#btn_translate").click(function(){
+        const url = 'https://stefanbohacek.com/hellosalut/?lang=' + $("INPUT#language_code").val();
+        $.get(url, function(data){
+            $("#hello").text(data.hello)
+        })
+    })
+    $('#language_code').keypress(function(e) {
+        if(e.keyCode == 13) {
+            e.preventDefault();
+            $('#btn_translate').click();
+        }
+    });
+});
